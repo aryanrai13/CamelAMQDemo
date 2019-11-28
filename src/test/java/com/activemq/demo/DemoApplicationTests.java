9a4ac94abfac06a0d84d0b25cb8f8cc1cf7d2120
deepak.rai <deepak.rai13@gmail.com>
2019-11-28 14:11:15 +0100

    new change

--- src/test/java/com/activemq/demo/DemoApplicationTests.java
+++ src/test/java/com/activemq/demo/DemoApplicationTests.java
@@ -1,6 +1,9 @@
 package com.activemq.demo;
 
+import com.activemq.demo.util.SplitMessage;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +12,45 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	String message = "GZXFRTJ675FTRHJJJ87zyxtBig Kumar           U000000000000017.450EURATZAT";
+	SplitMessage splitMessage = new SplitMessage(message);
+
+	@DisplayName("Check for Input Message Length")
 	@Test
-	public void contextLoads() {
+	public  void checkMessage() {
+		Assertions.assertEquals(splitMessage.inputString.length(),71);
 	}
+
+	@DisplayName("Check for TRN")
+	@Test
+	public  void checkTrn() {
+		Assertions.assertEquals(splitMessage.inputMessage.getTrn().trim().length(),22);
+	}
+
+	@DisplayName("Check for Format Type")
+	@Test
+	public  void checkFormatType() {
+		Assertions.assertEquals(splitMessage.inputMessage.getFormatType(),"U");
+	}
+
+	@DisplayName("Check for Currency")
+	@Test
+	public  void checkCurrency() {
+		Assertions.assertEquals(splitMessage.inputMessage.getCurrency().trim().length(),3);
+	}
+
+	@DisplayName("Check for Service")
+	@Test
+	public  void checkService() {
+		Assertions.assertEquals(splitMessage.inputMessage.getService().trim().length(),3);
+	}
+
+	@DisplayName("Check for Source Country Code")
+	@Test
+	public  void checkSourceCountryCode() {
+		Assertions.assertEquals(splitMessage.inputMessage.getSourceCountryCode().trim().length(),2);
+	}
+
+
 
 }
